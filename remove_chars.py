f7622ae214b0fd7accc3fb8cc4ca8342c992eaf9
Javier Lever <wilmerleve7@gmail.com>
2025-01-23 08:17:34 -0400

    modifications where made in remove_chars program

--- remove_chars.py
+++ remove_chars.py
@@ -1,10 +1,19 @@
 '''Write a Python code to remove characters from a string from 0 to n and return a new string.'''
-
+#my solution
 def remove_chars(my_string, my_num):
-    part_remove = my_string[:my_num + 1]
+    part_remove = my_string[:my_num]
     my_string = my_string.replace(part_remove, "")
     return my_string
 
-result = remove_chars("No x in Nixon", 7)
+result = remove_chars("No x in Nixon", 8)
 print(f"{result}")
 
+#solution
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
